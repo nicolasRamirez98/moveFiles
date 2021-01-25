f9dc4a348797b967969f58c9e6f4a800bbf6a90f
Nicolas Ramirez <46548508+nicolasRamirez98@users.noreply.github.com>
2021-01-25 18:58:04 -0500

    final

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PATH_ROOT="/usr/local/factura_electronica/data/100084"
-PATH_LOG="/var/log/cpFilesTransborder.log"
+PATH_DESTINI="/usr/local/factura_electronica/dmz-sftp-folder/100084-TRANSBORDER/output/graphical_representation"
 
 i=1;
 for dir in $(ls $PATH_ROOT | awk '{if ($1>="20200925" && $1<="20201023") print $1 }')
@@ -17,6 +17,8 @@ for dir in $(ls $PATH_ROOT | awk '{if ($1>="20200925" && $1<="20201023") print $
         echo "file: $file"
         echo "fileName: $fileName"
         echo " "
+
+        cp -v $PATH_ROOT/$directory/files/$file $PATH_DESTINI/$fileName.pdf
 
      ((i=i+1))
    done
